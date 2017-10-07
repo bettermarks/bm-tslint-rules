2e0de40ead35593902e0773fec2e3578180140b3
Christian Bewernitz <coder@karfau.de>
2017-10-07 14:23:00 +0200

    fixing false positives when imported path in modules path
    closes #3

--- src/noAbsoluteImportToOwnParentRule.ts
+++ src/noAbsoluteImportToOwnParentRule.ts
@@ -1,9 +1,19 @@
 import * as ts from 'typescript';
 import * as Lint from 'tslint';
 import {IOptions} from 'tslint';
+import * as path from 'path';
 
 export class Rule extends Lint.Rules.AbstractRule {
   public static FAILURE_STRING = 'importing parent path ';
+
+  private static _projectRoot: string;
+  static getProjectPath(): string {
+    if (!Rule._projectRoot) {
+      Rule._projectRoot = process.cwd();
+      console.info('no-absolute-import-to-own-parent assumes project path:', Rule._projectRoot);
+    }
+    return this._projectRoot;
+  }
 
   public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
     // tslint:disable-next-line:no-use-before-declare
@@ -13,12 +23,14 @@ export class Rule extends Lint.Rules.AbstractRule {
 
 // The walker takes care of all the work.
 class NoImportsWalker extends Lint.RuleWalker {
-  readonly absPathList: ReadonlyArray<string>;
+  readonly packagePathList: ReadonlyArray<string>;
+
   constructor(sourceFile: ts.SourceFile, options: IOptions) {
     super(sourceFile, options);
-    const full = sourceFile.fileName.split('/');
-    full.pop(); // get rid of module file name
-    this.absPathList = full;
+    const relative = path.relative(Rule.getProjectPath(), sourceFile.fileName);
+
+    // split and chop of source folder (1st element) and module file name (last element)
+    this.packagePathList = relative.split(path.sep).slice(1, -1); // get rid of source folder
   }
 
   public visitImportDeclaration(node: ts.ImportDeclaration) {
@@ -27,19 +39,13 @@ class NoImportsWalker extends Lint.RuleWalker {
 
     // we only check for relative paths that contain at least one slash
     if (imported[0] !== '.' && imported.indexOf('/') > -1) {
-      const importedList = imported.split('/');
-      const matchedFilePathIndex = this.absPathList.lastIndexOf(importedList[0]);
-      // first path part from moduleSpecifier was found in the path to the file.
-      if (matchedFilePathIndex > -1) {
-        /* so far it is matching, but one of the following path elements
-           could point to another directory
-         */
-        // mark that whole line as Error, we could easliy be more precise
-        // about where to put the marker, e.g. just at the path that shouldn't be used
+      const importedList = imported.split('/', 2);
+      // 1st part of import path matches with root package
+      if (importedList[0] === this.packagePathList[0]) {
         // TODO: providing a fix for this would be awesome, but I think it is not trivial
         // maybe we can deal with some easy cases, more complex stuff still needs hand work?
         this.addFailureAtNode(
-          node, `${Rule.FAILURE_STRING} ${imported} is not allowed.`
+          node.moduleSpecifier, `${Rule.FAILURE_STRING} ${imported} is not allowed.`
         );
       }
     }
