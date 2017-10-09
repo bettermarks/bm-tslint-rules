6bf13eaf8fda87550f6eef852f147e67734d8d28
Christian Bewernitz <coder@karfau.de>
2017-10-09 19:24:20 +0200

    simplification of code without the need to check out package.json
    0.0.7

--- ci-test-master-version.sh
+++ ci-test-master-version.sh
@@ -1,21 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
-# there shouldn't be a diff, otherwise checking out package.json from master is not safe
-git diff --exit-code > /dev/null || (echo "git workspace not clean, aborting" && exit 1)
-
 CURR_BRANCH=`(git symbolic-ref --short HEAD)`
 if [[ "master" = "$CURR_BRANCH" ]]
 then
   echo "current branch is master, not checking" && exit 0
 fi
-echo "current branch is $CURR_BRANCH"
-CURR_VERSION=`(node -p 'require("./package.json").version')`
+#echo "current branch is $CURR_BRANCH"
+CURR_VERSION=`(node -p "($(cat ./package.json)).version")`
 echo "current branch '$CURR_BRANCH' is at version '$CURR_VERSION'"
 
-git checkout origin/master -- package.json
-MASTER_VERSION=`(node -p 'require("./package.json").version')`
-git checkout HEAD -- package.json
+MASTER_VERSION=`(node -p "($(git show origin/master:package.json)).version")`
 echo "master branch is at version '$MASTER_VERSION'"
 if [[ "$CURR_VERSION" = "$MASTER_VERSION" ]]
 then
